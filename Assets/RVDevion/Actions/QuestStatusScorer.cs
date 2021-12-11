ed33da894a7a5709af3a572bbad8111d14cad09c
adrianre12 <adrianrevill@hotmail.com>
2021-12-11 15:20:06 +0000

    Setup Brut quest demo

---
+++ Assets/RVDevion/Actions/QuestStatusScorer.cs
@@ -0,0 +1,57 @@
+using DevionGames.QuestSystem;
+using RVModules.RVSmartAI.GraphElements;
+using UnityEngine;
+
+
+[System.Serializable]
+public class QuestStatusScorer : AiScorer
+{
+    [QuestPicker(true)]
+    [SerializeField]
+    protected Quest _quest;
+    [SerializeField]
+    protected float _inactive = (int)Status.Inactive;
+    [SerializeField]
+    protected float _active = (int)Status.Active;
+    [SerializeField]
+    protected float _completed = (int)Status.Completed;
+    [SerializeField]
+    protected float _failed = (int)Status.Failed;
+    [SerializeField]
+    protected float _canceled = (int)Status.Canceled;
+    [Tooltip("The value returned if the quest is not found")]
+    [SerializeField]
+    protected float _default = -1f;
+
+    public override float Score(float _deltaTime)
+    {
+        if (QuestManager.current.HasQuest(_quest, out Quest quest))
+        {
+            switch (quest.Status)
+            {
+                case Status.Inactive:
+                    {
+                        return _inactive;
+                    }
+                case Status.Active:
+                    {
+                        return _active;
+                    }
+                case Status.Completed:
+                    {
+                        return _completed;
+                    }
+                case Status.Failed:
+                    {
+                        return _failed;
+                    }
+                case Status.Canceled:
+                    {
+                        return _canceled;
+                    }
+            }
+        }
+        return _default;
+    }
+}
+
